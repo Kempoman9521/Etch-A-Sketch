8466f87dea7569edfae2e121f47342519c280bbe
Kempoman9521 <alanhazel3@yahoo.com>
2023-05-11 15:19:14 -0400

    changed within function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const mainContainer = document.querySelector("#main-container");
 
-function createContainers() {
+function addDivs() {
 for(let i = 0; i < 16; i++) {
     const container = document.createElement("div");
     container.classList.toggle("gridContainer");
-    mainContainer.appendChild(div);
+    mainContainer.appendChild(container);
 }
 }
-createContainers();
+addDivs();
