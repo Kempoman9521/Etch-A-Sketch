2d857d6fc2da739e8b17f09c29dc7e6c7bd33edb
Kempoman9521 <alanhazel3@yahoo.com>
2023-05-11 14:40:05 -0400

    change function name and classList name

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const mainContainer = document.querySelector("#main-container");
 
-function addDivs() {
-for(let i = 0; i <= 10; i++) {
+function createContainers() {
+for(let i = 0; i < 16; i++) {
     const div = document.createElement("div");
+    div.classList.toggle("grid");
     mainContainer.appendChild(div);
 }
 }
-
-addDivs();
+createContainers();
